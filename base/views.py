94f52f6d07ddb89408052275c467bd5ec18452ac
jaku988 <mineface98@gmail.com>
2023-10-15 20:46:51 +0200

    Logowanie i wylogowywanie uzytkownika

--- base/views.py
+++ base/views.py
@@ -1,28 +1,55 @@
 from django.shortcuts import render, redirect
-#from django.http import HttpResponse
+from django.db.models import Q
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm, TopicForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
-#rooms = [
-#    {'id': 1, 'name': 'Lets learn python!'},
-#    {'id': 2, 'name': 'Design with me'},
-#    {'id': 3, 'name': 'Frondend Dev'},
-#]
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(password, login)
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password does not exist!')
+
+    context = {}
+    return render(request, 'base/login_registration.html', context)
+
+def logout_user(request):
+
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
 
-    q = request.GET.get('q')
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    if q == None:
-        rooms = Room.objects.all()
-    else:
-        rooms = Room.objects.filter(topic__name__icontains=q)
-
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
     context = {'rooms': rooms,
-               'topics': topics}
+               'topics': topics,
+               'room_count': room_count}
 
     return render(request, 'base/home.html',  context)
 
